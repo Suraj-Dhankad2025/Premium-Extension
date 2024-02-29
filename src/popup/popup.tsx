4e40b459fff41109ac8ccc9dd9582a38e740fcfa
suraj dhankad <suraj.dhanked@gmail.com>
2024-02-29 20:26:14 +0530

    added all data

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,11 +1,59 @@
-import React from "react";
-import './popup.css'
-
+import React, { useEffect, useState } from "react";
+import './popup.css';
+import problemToCompanyMatcher from '../../resources/company-wise-problem-list.js';
 const Popup = () => {
+    const [currentTabURL, setCurrentTabURL] = useState('');
+    const [companies, setCompanies] = useState([]);
+
+    useEffect(() => {
+        function getCurrentTabURL() {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                if (tabs.length > 0) {
+                    setCurrentTabURL(tabs[0].url);
+                }
+            });
+        }
+
+        getCurrentTabURL();
+
+        chrome.tabs.onActivated.addListener(getCurrentTabURL);
+
+        return () => {
+            chrome.tabs.onActivated.removeListener(getCurrentTabURL);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (currentTabURL.includes("leetcode")) {
+            const startIndex = currentTabURL.indexOf("problems/") + "problems/".length;
+            const endIndex = currentTabURL.indexOf("/description");
+            const extractedString = currentTabURL.substring(startIndex, endIndex);
+
+            const companiesData = problemToCompanyMatcher["leetcode.com"][extractedString];
+            if (companiesData && companiesData.length > 0) {
+                const companiesArray = companiesData.map(companyData => ({
+                    company: companyData.company,
+                    frequency: companyData.num_occur
+                }));
+                setCompanies(companiesArray);
+            } else {
+                setCompanies([]);
+            }
+        } else {
+            setCompanies([{ company: "No Problem Found!", frequency: 0 }]);
+        }
+    }, [currentTabURL]);
+
     return (
-        <div>
-            <h1 className="text-4xl text-green-500">Hello World</h1>
-        </div>
+        <div className="bg-slate-300 p-3">
+        <ul>
+            {companies.map((companyData, index) => (
+                <li key={index} className="text-sm inline-block rounded-full text-white bg-gray-800 px-4 py-2 mr-2 mb-2 hover:bg-gray-600 cursor-pointer">
+                    {companyData.company} <span><strong>{companyData.frequency}</strong></span>
+                </li>
+            ))}
+        </ul>
+    </div>
     )
 };
 
